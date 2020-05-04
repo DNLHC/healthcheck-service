57bc56d8b480d17a119cf1c41278f7254a3ee324
DNLHC <gnz.danila@gmail.com>
2020-05-04 15:36:59 +0300

    e2e tests for modifying listing and deleting checks

--- __test__/e2e.test.js
+++ __test__/e2e.test.js
@@ -62,4 +62,74 @@ describe('Checks API', () => {
       expect(response.body.error).toBeDefined();
     });
   });
+
+  describe('Modifying checks', () => {
+    it('Modifies check', async () => {
+      const fakeCheck = await createFakeCheck({ name: 'inital' });
+      await checksDb.insert(fakeCheck);
+      const response = await request.patch(`checks/${fakeCheck.id}`, {
+        json: { name: 'changed' },
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.success).toBe(true);
+      expect(response.body.data.name).toBe('changed');
+
+      return checksDb.remove({ id: fakeCheck.id });
+    });
+
+    it('Resets resource', async () => {
+      const fakeCheck = await createFakeCheck({
+        url: 'http://ya.ru',
+        statusBefore: 200,
+        statusAfter: 200,
+        requsetTime: 15,
+      });
+      await checksDb.insert(fakeCheck);
+      const response = await request.patch(`checks/${fakeCheck.id}`, {
+        json: { url: 'http://google.com' },
+      });
+      const updatedCheck = response.body.data;
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.success).toBe(true);
+      expect(updatedCheck.statusBefore).toBeNull();
+      expect(updatedCheck.statusAfter).toBeNull();
+      expect(updatedCheck.requestTime).toBeNull();
+
+      return checksDb.remove({ id: fakeCheck.id });
+    });
+  });
+
+  describe('Listing checks', () => {
+    it('Lists checks', async () => {
+      const fakeChecks = await Promise.all(
+        Array.from({ length: 2 }, () => createFakeCheck())
+      );
+      const insertedChecks = await Promise.all(fakeChecks.map(checksDb.insert));
+      const response = await request.get('checks');
+
+      expect.assertions(2 + fakeChecks.length);
+      expect(response.statusCode).toBe(200);
+      expect(response.body.success).toBe(true);
+
+      insertedChecks.forEach((check) => {
+        expect(response.body.data).toContainEqual(check);
+      });
+
+      return Promise.all(insertedChecks.map(checksDb.remove));
+    });
+  });
+
+  describe('Deleting checks', () => {
+    it('Deletes check', async () => {
+      const fakeCheck = await createFakeCheck();
+      await checksDb.insert(fakeCheck);
+      const response = await request.delete(`checks/${fakeCheck.id}`);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.success).toBe(true);
+      expect(response.body.data.deletedCount).toBe(1);
+    });
+  });
 });
--- src/controllers/delete-check.js
+++ src/controllers/delete-check.js
@@ -1,6 +1,6 @@
 export default function createDeleteCheck({ removeCheck }) {
   return async function (httpRequest) {
-    const result = await removeCheck({ id: httpRequest.parmas.id });
+    const result = await removeCheck({ id: httpRequest.params.id });
 
     return {
       statusCode: result.deletedCount === 0 ? 404 : 200,
