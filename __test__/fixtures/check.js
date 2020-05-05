c27954f9c8acf08c5e98a68089820c0970e64e1f
DNLHC <gnz.danila@gmail.com>
2020-05-06 01:20:17 +0300

    generate valid name to prevent random test fails

--- __test__/fixtures/check.js
+++ __test__/fixtures/check.js
@@ -6,12 +6,18 @@ function md5(string) {
   return crypto.createHash('md5').update(string, 'utf8').digest('hex');
 }
 
+// Ensure that generated name is longer than 2 characters
+function createName() {
+  const name = faker.internet.domainName();
+  return name.length > 2 ? name : createName();
+}
+
 export default async function createFakeCheck(overrides = {}) {
   const id = await nanoid();
 
   const check = {
     id,
-    name: faker.internet.domainWord(),
+    name: createName(),
     url: faker.internet.url(),
     active: faker.random.boolean(),
     createdAt: Date.now(),
